79d606c3850b73143194e527c53a05fb160b1249
danssolutions <danssolutions@protonmail.com>
2021-01-26 13:21:56 +0200

    added new token info funcs to CCTokenTags.h

--- src/cc/CCTokenTags.h
+++ src/cc/CCTokenTags.h
@@ -19,6 +19,16 @@
 
 #include "CCinclude.h"
 
+/// Returns pubkeys that have/had possession of the specified tokenid.
+/// @param tokenid id of token to check for
+/// @param minbalance if set, only pubkeys that have this many tokens of this id will be returned. If 0, lists every pubkey that ever has/had tokens with this id
+UniValue TokenOwners(uint256 tokenid, int64_t minbalance);
+
+/// Returns tokenids of tokens that the specified pubkey is in possession of.
+/// @param pk pubkey to check for tokens
+/// @param minbalance minimum balance of tokens for pubkey required for its id to be added to the array. If 0, lists ids of every token that the pubkey ever has/had
+UniValue TokenInventory(const CPubKey pk, int64_t minbalance);
+
 bool TokenTagsValidate(struct CCcontract_info *cp, Eval* eval, const CTransaction &tx, uint32_t nIn);
 
 #endif
